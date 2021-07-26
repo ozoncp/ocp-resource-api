9e3359cb6c544b7ae21a7c1f7ffdd3607b63b511
Ilya Tyaptin <tyaptinilia@gmail.com>
2021-07-26 17:08:33 +0400

    task-3: Add collection utils for resource slices
    - Split slice to batch
    - Convert slice to batch by id

--- internal/utils/collection.go
+++ internal/utils/collection.go
@@ -2,7 +2,13 @@ package utils
 
 import (
 	"errors"
+	"github.com/ozoncp/ocp-resource-api/internal/models"
 )
+
+var ErrBatchSizeIsNull = errors.New("batch size should be greater that 0")
+var ErrSourceIsNil = errors.New("source should not be nil")
+var ErrDuplicatedKey = errors.New("key should not be duplicated in result map")
+var ErrFilterOutValuesIsNil = errors.New("filterOutValues should not be nil")
 
 func sliceContainsIn(source []int, value int) (bool, error) {
 	for i := 0; i < len(source); i++ {
@@ -15,10 +21,10 @@ func sliceContainsIn(source []int, value int) (bool, error) {
 
 func FilterOutInt(source []int, filterOutValues []int) ([]int, error) {
 	if source == nil {
-		return nil, errors.New("source should not be nil")
+		return nil, ErrSourceIsNil
 	}
 	if filterOutValues == nil {
-		return nil, errors.New("filterOutValues should not be nil")
+		return nil, ErrFilterOutValuesIsNil
 	}
 	result := make([]int, 0, len(source))
 	for i := 0; i < len(source); i++ {
@@ -30,23 +36,7 @@ func FilterOutInt(source []int, filterOutValues []int) ([]int, error) {
 	return result, nil
 }
 
-func SplitInt(source []int, batchSize int) ([][]int, error) {
-	if batchSize <= 0 {
-		return nil, errors.New("batch size should be greater that 0")
-	}
-	if source == nil {
-		return nil, errors.New("source should not be nil")
-	}
-	batchCount := calcChunkSize(source, batchSize)
-	result := make([][]int, 0, batchCount)
-	for i := 0; i < batchCount; i++ {
-		start, end := batchBounds(len(source), batchSize, i)
-		result = append(result, source[start:end])
-	}
-	return result, nil
-}
-
-func batchBounds(length int, batchSize int, i int) (int, int) {
+func batchBounds(length uint, batchSize uint, i uint) (uint, uint) {
 	start := i * batchSize
 	end := (i + 1) * batchSize
 	if end > length {
@@ -55,9 +45,9 @@ func batchBounds(length int, batchSize int, i int) (int, int) {
 	return start, end
 }
 
-func calcChunkSize(source []int, batchSize int) int {
-	batchCount := len(source) / batchSize
-	if len(source)%batchSize != 0 {
+func calcChunkSize(sourceLength uint, batchSize uint) uint {
+	batchCount := sourceLength / batchSize
+	if sourceLength%batchSize != 0 {
 		batchCount += 1
 	}
 	return batchCount
@@ -65,15 +55,63 @@ func calcChunkSize(source []int, batchSize int) int {
 
 func ReverseMapIntToInt(source map[int]int, forceRewrite bool) (map[int]int, error) {
 	if source == nil {
-		return nil, errors.New("source should not be nil")
+		return nil, ErrSourceIsNil
 	}
 	result := make(map[int]int, len(source))
 	for key, value := range source {
 		_, ok := result[value]
 		if ok && !forceRewrite {
-			return nil, errors.New("key should not be duplicated in result map")
+			return nil, ErrDuplicatedKey
 		}
 		result[value] = key
+	}
+	return result, nil
+}
+
+func SplitToBulksInt(source []int, batchSize uint) ([][]int, error) {
+	if batchSize == 0 {
+		return nil, ErrBatchSizeIsNull
+	}
+	if source == nil {
+		return nil, ErrSourceIsNil
+	}
+	batchCount := calcChunkSize(uint(len(source)), batchSize)
+	result := make([][]int, 0, batchCount)
+	for i := uint(0); i < batchCount; i++ {
+		start, end := batchBounds(uint(len(source)), batchSize, i)
+		result = append(result, source[start:end])
+	}
+	return result, nil
+}
+
+func SplitToBulksResource(source []models.Resource, batchSize uint) ([][]models.Resource, error) {
+	if batchSize == 0 {
+		return nil, ErrBatchSizeIsNull
+	}
+	if source == nil {
+		return nil, ErrSourceIsNil
+	}
+	sourceLength := uint(len(source))
+	batchCount := calcChunkSize(sourceLength, batchSize)
+	result := make([][]models.Resource, 0, batchCount)
+	for i := uint(0); i < batchCount; i++ {
+		start, end := batchBounds(sourceLength, batchSize, i)
+		result = append(result, source[start:end])
+	}
+	return result, nil
+}
+
+func SliceToMapResource(source []models.Resource, forceRewrite bool) (map[uint64]models.Resource, error) {
+	if source == nil {
+		return nil, ErrSourceIsNil
+	}
+	result := make(map[uint64]models.Resource, len(source))
+	for _, resource := range source {
+		_, ok := result[resource.Id]
+		if ok && !forceRewrite {
+			return nil, ErrDuplicatedKey
+		}
+		result[resource.Id] = resource
 	}
 	return result, nil
 }
--- internal/utils/collection_test.go
+++ internal/utils/collection_test.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"errors"
+	"github.com/ozoncp/ocp-resource-api/internal/models"
 	"testing"
 )
 
@@ -33,6 +34,33 @@ func sliceIntEquals(a []int, b []int) bool {
 	return true
 }
 
+func sliceResourceEquals(a []models.Resource, b []models.Resource) bool {
+	if !equalsNilValues(a, b) || len(a) != len(b) {
+		return false
+	}
+	for i := 0; i < len(a); i++ {
+		if a[i].Id != b[i].Id ||
+			a[i].UserId != b[i].UserId ||
+			a[i].Status != b[i].Status ||
+			a[i].Type != b[i].Type {
+			return false
+		}
+	}
+	return true
+}
+
+func sliceResourceMatrixEquals(a [][]models.Resource, b [][]models.Resource) bool {
+	if !equalsNilValues(a, b) || len(a) != len(b) {
+		return false
+	}
+	for i := 0; i < len(a); i++ {
+		if !sliceResourceEquals(a[i], b[i]) {
+			return false
+		}
+	}
+	return true
+}
+
 func mapIntIntEquals(a map[int]int, b map[int]int) bool {
 	if !equalsNilValues(a, b) || len(a) != len(b) {
 		return false
@@ -40,6 +68,22 @@ func mapIntIntEquals(a map[int]int, b map[int]int) bool {
 	for key, aValue := range a {
 		bValue := b[key]
 		if aValue != bValue {
+			return false
+		}
+	}
+	return true
+}
+
+func mapUInt64ResourceEquals(a map[uint64]models.Resource, b map[uint64]models.Resource) bool {
+	if !equalsNilValues(a, b) || len(a) != len(b) {
+		return false
+	}
+	for key, aValue := range a {
+		bValue := b[key]
+		if aValue.Id != bValue.Id ||
+			aValue.UserId != bValue.UserId ||
+			aValue.Status != bValue.Status ||
+			aValue.Type != bValue.Type {
 			return false
 		}
 	}
@@ -56,8 +100,8 @@ func errorEquals(a error, b error) bool {
 	return a.Error() == b.Error()
 }
 
-func assertSplitInt(t *testing.T, source []int, batchSize int, expectedSlice [][]int, expectedErr error) {
-	actualSlice, actualErr := SplitInt(source, batchSize)
+func assertSplitToBulksInt(t *testing.T, source []int, batchSize uint, expectedSlice [][]int, expectedErr error) {
+	actualSlice, actualErr := SplitToBulksInt(source, batchSize)
 	if !errorEquals(actualErr, expectedErr) {
 		t.Fatalf("Error assertion failed. Actual '%v'. Expected '%v'", actualErr, expectedErr)
 	}
@@ -86,24 +130,25 @@ func assertMapReverseIntInt(t *testing.T, source map[int]int, rewrite bool, expe
 	}
 }
 
-func TestSplitInt(t *testing.T) {
-	assertSplitInt(t, []int{1, 2, 3, 4, 5, 6}, 5, [][]int{{1, 2, 3, 4, 5}, {6}}, nil)
+// TODO rewrite old tests to a new format
+func TestSplitToBulksInt(t *testing.T) {
+	assertSplitToBulksInt(t, []int{1, 2, 3, 4, 5, 6}, 5, [][]int{{1, 2, 3, 4, 5}, {6}}, nil)
 }
 
-func TestSplitIntNil(t *testing.T) {
-	assertSplitInt(t, nil, 5, nil, errors.New("source should not be nil"))
+func TestSplitToBulksIntNil(t *testing.T) {
+	assertSplitToBulksInt(t, nil, 5, nil, ErrSourceIsNil)
 }
 
-func TestSplitIntBatchSizeZero(t *testing.T) {
-	assertSplitInt(t, []int{1}, 0, nil, errors.New("batch size should be greater that 0"))
+func TestSplitToBulksIntBatchSizeZero(t *testing.T) {
+	assertSplitToBulksInt(t, []int{1}, 0, nil, ErrBatchSizeIsNull)
 }
 
-func TestSplitIntBatchWithSameLength(t *testing.T) {
-	assertSplitInt(t, []int{1, 2, 3, 4, 5}, 5, [][]int{{1, 2, 3, 4, 5}}, nil)
+func TestSplitToBulksIntBatchWithSameLength(t *testing.T) {
+	assertSplitToBulksInt(t, []int{1, 2, 3, 4, 5}, 5, [][]int{{1, 2, 3, 4, 5}}, nil)
 }
 
-func TestSplitIntBatchGreaterThanLenght(t *testing.T) {
-	assertSplitInt(t, []int{1}, 5, [][]int{{1}}, nil)
+func TestSplitToBulksIntBatchGreaterThanLength(t *testing.T) {
+	assertSplitToBulksInt(t, []int{1}, 5, [][]int{{1}}, nil)
 }
 
 func TestFilterOutInt(t *testing.T) {
@@ -111,12 +156,12 @@ func TestFilterOutInt(t *testing.T) {
 }
 
 func TestFilterOutIntSourceNil(t *testing.T) {
-	assertFilterOut(t, nil, []int{1, 2, 3, 5}, nil, errors.New("source should not be nil"))
+	assertFilterOut(t, nil, []int{1, 2, 3, 5}, nil, ErrSourceIsNil)
 }
 
 func TestFilterOutIntFilterValuesNil(t *testing.T) {
 	assertFilterOut(t, []int{1, 2, 3, 5}, nil,
-		nil, errors.New("filterOutValues should not be nil"))
+		nil, ErrFilterOutValuesIsNil)
 }
 
 func TestReverseMapIntToInt(t *testing.T) {
@@ -124,13 +169,130 @@ func TestReverseMapIntToInt(t *testing.T) {
 }
 
 func TestReverseMapIntToIntNilMap(t *testing.T) {
-	assertMapReverseIntInt(t, nil, false, nil, errors.New("source should not be nil"))
+	assertMapReverseIntInt(t, nil, false, nil, ErrSourceIsNil)
 }
 
 func TestReverseMapIntToIntDuplicate(t *testing.T) {
-	assertMapReverseIntInt(t, map[int]int{1: 2, 3: 2}, false, nil, errors.New("key should not be duplicated in result map"))
+	assertMapReverseIntInt(t, map[int]int{1: 2, 3: 2}, false, nil, ErrDuplicatedKey)
 }
 
 func TestReverseMapIntToIntDuplicateRewrite(t *testing.T) {
 	assertMapReverseIntInt(t, map[int]int{1: 2, 3: 2}, true, map[int]int{2: 3}, nil)
+}
+
+func TestSplitToBulksResource(t *testing.T) {
+	type args struct {
+		slice     []models.Resource
+		batchSize uint
+	}
+	testSlice := []models.Resource{
+		models.NewResource(1, 1, 1, 1),
+		models.NewResource(2, 2, 2, 2),
+		models.NewResource(3, 3, 3, 3),
+		models.NewResource(4, 4, 4, 4),
+	}
+
+	tests := []struct {
+		name          string
+		args          args
+		expectedSlice [][]models.Resource
+		expectedError error
+	}{
+		{
+			name: "base",
+			args: args{slice: testSlice, batchSize: uint(3)},
+			expectedSlice: [][]models.Resource{
+				testSlice[0:3],
+				testSlice[3:],
+			},
+			expectedError: nil,
+		},
+		{
+			name:          "batch greater than slice len",
+			args:          args{slice: testSlice, batchSize: uint(5)},
+			expectedSlice: [][]models.Resource{testSlice},
+			expectedError: nil,
+		},
+		{
+			name:          "slice is nil",
+			args:          args{slice: nil, batchSize: 10},
+			expectedSlice: nil,
+			expectedError: ErrSourceIsNil,
+		},
+		{
+			name:          "batchSize is 0",
+			args:          args{slice: testSlice, batchSize: 0},
+			expectedSlice: nil,
+			expectedError: ErrBatchSizeIsNull,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual, err := SplitToBulksResource(test.args.slice, test.args.batchSize)
+			if !equalsNilValues(err, test.expectedError) && !errors.Is(err, test.expectedError) {
+				t.Errorf("Error from SplitToBulksResource() = %v, expected %v", actual, test.expectedSlice)
+			}
+			if !sliceResourceMatrixEquals(actual, test.expectedSlice) {
+				t.Errorf("SplitToBulksResource() = %v, expected %v", actual, test.expectedSlice)
+			}
+		})
+	}
+}
+
+func TestSliceToMapResource(t *testing.T) {
+	testSlice := []models.Resource{
+		models.NewResource(1, 1, 1, 1),
+		models.NewResource(2, 2, 2, 2),
+		models.NewResource(3, 3, 3, 3),
+		models.NewResource(1, 4, 4, 4),
+	}
+
+	baseMap := map[uint64]models.Resource{
+		1: models.NewResource(1, 4, 4, 4),
+		2: models.NewResource(2, 2, 2, 2),
+		3: models.NewResource(3, 3, 3, 3),
+	}
+
+	type args struct {
+		source       []models.Resource
+		forceRewrite bool
+	}
+	tests := []struct {
+		name        string
+		args        args
+		expected    map[uint64]models.Resource
+		expectedErr error
+	}{
+		{
+			name:        "with force rewrite",
+			args:        args{source: testSlice, forceRewrite: true},
+			expected:    baseMap,
+			expectedErr: nil,
+		},
+		{
+			name:        "without force rewrite and duplicated keys",
+			args:        args{source: testSlice, forceRewrite: false},
+			expected:    nil,
+			expectedErr: ErrDuplicatedKey,
+		},
+		{
+			name:        "with nil source",
+			args:        args{source: nil, forceRewrite: true},
+			expected:    nil,
+			expectedErr: ErrSourceIsNil,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual, err := SliceToMapResource(test.args.source, test.args.forceRewrite)
+			if !equalsNilValues(err, test.expectedErr) && !errors.Is(err, test.expectedErr) {
+				t.Errorf("SliceToMapResource() error = %v, expected %v", err, test.expectedErr)
+				return
+			}
+			if !mapUInt64ResourceEquals(actual, test.expected) {
+				t.Errorf("SliceToMapResource() actual = %v, expected %v", actual, test.expected)
+			}
+		})
+	}
 }
