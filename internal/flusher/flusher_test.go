e3b73e294174792ae1f96c9e663eea182aab3c41
Ilya Tyaptin <tyaptinilia@gmail.com>
2021-07-28 16:39:30 +0400

    task-4: Add test suit and mocks for flusher and repo

---
+++ internal/flusher/flusher_test.go
@@ -0,0 +1,12 @@
+package flusher_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/ozoncp/ocp-resource-api/internal/flusher"
+)
+
+var _ = Describe("Flusher", func() {
+
+})
